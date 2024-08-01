5f5ca07d8c3de818a2ad5424853eeb295e1b0e2c
Trisha <tshukla025@gmail.com>
2024-08-01 14:44:55 +0530

    Dark Mode added

---
+++ index.js
@@ -0,0 +1,18 @@
+let toggle_cont=document.getElementById("box");
+let circle=document.getElementById("circle");
+let body=document.getElementsByTagName("body");
+console.log(body);
+let value=false;
+
+toggle_cont.addEventListener("click",()=>{
+    if(!value){
+        circle.classList.add("movement");
+        body[0].classList.add("bg-black");
+        value=true;
+    }
+    else{
+        circle.classList.remove("movement");
+        body[0].classList.remove("bg-black");
+        value=false;
+    }
+})
